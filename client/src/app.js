1820130b09665c41707cda3309638970d46e2e38
Josephine Elder <parusmontanus@gmail.com>
2017-01-10 14:20:03 +0000

    Setting up components files

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var MovieContainer = require('./containers/MovieContainer');
+
 window.onload = function(){
   ReactDOM.render(
-    <h1> App Started </h1>,
+    <MovieContainer />,
     document.getElementById('app')
   );
 }
---
+++ client/src/containers/MovieContainer.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+// var MovieList = require('../components/MovieList.jsx');
+// var Movie = require('../components/Movie.jsx');
+
+var MovieContainer = React.createClass({
+
+
+
+
+  render: function() {
+    return (
+      <h1>Moooooooooooovies</h1>
+    );
+  }  
+
+
+
+
+});
+
+module.exports = MovieContainer;
